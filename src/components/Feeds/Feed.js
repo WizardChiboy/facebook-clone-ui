56e508bc637cb003e04d99710f7a9df6481a9d87
wizard chiboy <doncodesacademy@gmail.com>
2022-05-22 22:13:23 +0100

    some edits

--- src/components/Feeds/Feed.js
+++ src/components/Feeds/Feed.js
@@ -10,15 +10,28 @@ import {
 import "./Feed.css";
 import Story from "./Story/Story";
 
+import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
+
 function Feed() {
   return (
     <>
       <div className="feeds">
-        <Story
-          username="Don martins"
-          profilePhoto={ProfilePhoto}
-          storyImg={ProfilePhoto}
-        />
+        <div className="feeds__create__story">
+          <img
+            src={ProfilePhoto}
+            alt="story"
+            className="feeds__create__story__img"
+          />
+          <div className="feeds__create__story__icon_container">
+            <div className="feeds__create__story__icon_container__icon">
+              <AddCircleOutlineRoundedIcon />
+            </div>
+
+            <div className="feeds__create__story__icon_container__text">
+              <h4>Create Story</h4>
+            </div>
+          </div>
+        </div>
 
         <Story
           username="ronaldo"
